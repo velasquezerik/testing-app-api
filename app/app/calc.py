e085b7b18ee2e1e8ed389023073f4b60a0ee2f84
Erik Velasquez <erikvelasquez.25@gmail.com>
2020-05-21 13:35:58 -0400

    Complete Custom User Model

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,6 +2,7 @@ def add(x, y):
     """Add two numbers together and return the result"""
     return x + y
 
+
 def subtract(x, y):
     """Subtract x from y and return result"""
     return (y - x)
--- app/core/models.py
+++ app/core/models.py
@@ -7,11 +7,24 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new User"""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
+
+    def create_superuser(self, email, password):
+        """Creates and saves a new super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""

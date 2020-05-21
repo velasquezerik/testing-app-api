602401a487ba83fa3831f386e75e2195cff46966
Erik Velasquez <erikvelasquez.25@gmail.com>
2020-05-21 13:16:25 -0400

    Improve code style

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-    PermissionsMixin
+                                        PermissionsMixin
 
 
 class UserManager(BaseUserManager):
@@ -12,7 +12,6 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
-
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
--- app/core/tests/test_model.py
+++ app/core/tests/test_model.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
@@ -9,9 +8,9 @@ class ModelTests(TestCase):
         email = 'test@londonappdev.com'
         password = 'Password123'
         user = get_user_model().objects.create_user(
-			email=email,
-			password=password
-		)
+            email=email,
+            password=password
+        )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
